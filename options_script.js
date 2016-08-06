0fa5954f67b94cec9b1c9633ca1b0f80a7198cc2
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-08-06 22:09:22 +0900

    [Add] 設定ページ

---
+++ options_script.js
@@ -0,0 +1,151 @@
+const tbody = document.getElementById("tbody");
+
+const settingEntrys = [{
+	name: "対象タイトル（メモ用）",
+	key: "target-title",
+	format: "Exampleニュース"
+}, {
+	name: "対象URL（先頭一致）",
+	key: "target-url",
+	format: "http://example.com/article/"
+}, {
+	name: "対象要素",
+	key: "target-element",
+	format: ".pageProperty, #main, .pagenator"
+}, {
+	name: "次ページのリンク要素",
+	key: "next-page-link",
+	format: "a.pageNext"
+}, {
+	name: "保存"
+}, {
+	name: "削除"
+}];
+
+addSettingHeader();
+loadSetting().then(settings => {
+	Object.keys(settings).sort().forEach(id => {
+		const setting = settings[id];
+		addSettingElement(id, setting);
+	});
+}).then(addNewSettingElement);
+
+document.getElementById("add").addEventListener("click", addNewSettingElement);
+
+
+function addSettingHeader() {
+	const tr = document.createElement("tr");
+	settingEntrys.forEach(entry => {
+		const th = document.createElement("th");
+		th.innerText = entry.name;
+		tr.appendChild(th);
+	});
+	tbody.appendChild(tr);
+}
+
+function addNewSettingElement() {
+	addSettingElement();
+}
+function addSettingElement(id, setting) {
+	const tr = document.createElement("tr");
+	if (id) tr.setAttribute("data-id", id)
+
+	const fields = settingEntrys.filter(_ => _.key).map(entry => {
+		const key = entry.key;
+		const td = document.createElement("td");
+		const input = document.createElement("input");
+		input.setAttribute("data-key", key);
+		input.placeholder = entry.format;
+		if (setting) input.value = setting[key];
+		td.appendChild(input);
+		tr.appendChild(td);
+		return {key, input}
+	});
+	
+	{
+		const button = document.createElement("input");
+		button.type = "button";
+		button.value = "保存";
+		button.disabled = true;
+
+		let savedValue = {};
+		const changedSet = new Set();
+		fields.forEach(({key, input}) => {
+			savedValue[key] = input.value;
+			input.addEventListener("change", () => {
+				if (savedValue[key] === input.value) {
+					changedSet.add(key);
+				} else {
+					changedSet.delete(key);
+				}
+				button.disabled = changedSet.size !== 0;
+			});
+		});
+
+		button.addEventListener("click", evt => {
+			const setting = {};
+			fields.forEach(({key, input}) => {
+				setting[key] = input.value;
+			});
+			saveSetting(setting, id).then(createdId => {
+				id = createdId;
+				tr.setAttribute("data-id", id);
+
+				savedValue = setting;
+				changedSet.clear();
+				button.disabled = true;
+			});
+		});
+
+		const td = document.createElement("td");
+		td.appendChild(button);
+		tr.appendChild(td);
+	}
+
+	{
+		const button = document.createElement("input");
+		button.type = "button";
+		button.value = "削除";
+		button.addEventListener("click", () => {
+			if (id) {
+				if (window.confirm("削除しますか？")) {
+					removeSetting(id).then(() => {
+						tbody.removeChild(tr);
+					});
+				}
+			} else {
+				tbody.removeChild(tr);
+			}
+		});
+
+		const td = document.createElement("td");
+		td.appendChild(button);
+		tr.appendChild(td);
+	}
+
+	tbody.appendChild(tr);
+}
+
+function loadSetting() {
+	return new Promise(resolve => {
+		chrome.storage.local.get(items => {
+			resolve(items);
+		});
+	});
+}
+
+function saveSetting(setting, id = Date.now().toString()) {
+	return new Promise(resolve => {
+		chrome.storage.local.set({
+			[id]: setting
+		}, () => {
+			resolve(id);
+		});
+	});
+}
+
+function removeSetting(id) {
+	return new Promise(resolve => {
+		chrome.storage.local.remove(id, resolve);
+	});
+}
